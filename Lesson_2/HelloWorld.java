b511470146d407d0860a293ea4bc5abe9864d9ce
Shamil K <shajdaro@kpfu.ru>
2019-09-20 11:19:02 +0300

    added helloworld

---
+++ Lesson_2/HelloWorld.java
@@ -0,0 +1,35 @@
+// package Lesson_2;
+
+
+//  public, private, protected, defualt 
+
+
+
+/*
+boolean: true : false
+целочисленные:
+byte short int long char:  [-128; 127] [-32768 32767] [-2147483648 2147483647] [] [0; 65535]
+вещественные 
+float double
+
+
+*/
+
+
+
+/**
+* @author Shamil K
+* 
+*
+*/
+
+public class HelloWorld {
+
+	public static void main(String [] args) {
+		System.out.println("Hello, World!");
+		
+		System.out.print("abc");
+	}
+	
+	
+}
