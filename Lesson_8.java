f68463cdd1f2f247e4dab33056f1dcfbb82d7c82
Shamil K <shajdaro@kpfu.ru>
2019-10-11 10:43:23 +0300

    add l8

---
+++ Lesson_8.java
@@ -0,0 +1,36 @@
+/**
+* @author Shamil K
+* 
+* Двумерные массивы.
+*/
+
+public class Lesson_8 {
+
+
+	public static int [][] random2DArray(int m, int n){
+		int [][] array = new int [m][n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				array[i][j] = (int) (Math.random() * 100);
+			}
+		}
+		return array;
+	}
+	
+	public static String toString(int [][] matr) {
+		String result = "";
+		for (int i = 0; i < matr.length; i++) {
+			for (int j = 0; j < matr[0].length; j++) {
+				result += String.format("%4d", matr[i][j]);
+			}
+			result += "\n";
+		}
+		return result;
+	}
+
+	public static void main(String [] args) {
+		System.out.println(toString(random2DArray(5, 5)));
+	}
+	
+	
+}
